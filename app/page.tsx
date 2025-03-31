fc3758622b71ca45eb78dd470b14311f9dc7bfed
*vivek <vrvivekofficial@gmail.com>
2025-03-31 21:39:56 +0530

    add spotlight

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
+import Hero from "@/components/Hero";
 import React from "react";
 
 const page = () => {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10">
       <div className="max-w-7xl w-full">
-        <h1>heloo</h1>
+        <Hero />
       </div>
     </main>
   );
