36dabf946d51f288f41af03a2aabd9191cf57708
*vivek <vrvivekofficial@gmail.com>
2025-04-02 22:42:33 +0530

    create Hero icons

--- components/ui/Spotlight.tsx
+++ components/ui/Spotlight.tsx
@@ -15,8 +15,7 @@ export const Spotlight = ({ className, fill }: SpotlightProps) => {
       )}
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 3787 2842"
-      fill="none"
-    >
+      fill="none">
       <g filter="url(#filter)">
         <ellipse
           cx="1924.71"
@@ -25,8 +24,7 @@ export const Spotlight = ({ className, fill }: SpotlightProps) => {
           ry="273.501"
           transform="matrix(-0.822377 -0.568943 -0.568943 0.822377 3631.88 2291.09)"
           fill={fill || "white"}
-          fillOpacity="0.21"
-        ></ellipse>
+          fillOpacity="0.21"></ellipse>
       </g>
       <defs>
         <filter
@@ -36,19 +34,16 @@ export const Spotlight = ({ className, fill }: SpotlightProps) => {
           width="3785.16"
           height="2840.26"
           filterUnits="userSpaceOnUse"
-          colorInterpolationFilters="sRGB"
-        >
+          colorInterpolationFilters="sRGB">
           <feFlood floodOpacity="0" result="BackgroundImageFix"></feFlood>
           <feBlend
             mode="normal"
             in="SourceGraphic"
             in2="BackgroundImageFix"
-            result="shape"
-          ></feBlend>
+            result="shape"></feBlend>
           <feGaussianBlur
             stdDeviation="151"
-            result="effect1_foregroundBlur_1065_8"
-          ></feGaussianBlur>
+            result="effect1_foregroundBlur_1065_8"></feGaussianBlur>
         </filter>
       </defs>
     </svg>
