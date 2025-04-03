9f6a4db1289682aa01fd0ab64fc52799528fa70c
*vivek <vrvivekofficial@gmail.com>
2025-04-03 22:29:49 +0530

    create navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 import Hero from "@/components/Hero";
+import { FloatingNav } from "@/components/ui/FloatingNavbar";
+import { navItems } from "@/data";
 import React from "react";
 
 const page = () => {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10">
       <div className="max-w-7xl w-full">
+        <FloatingNav navItems={navItems}/>
         <Hero />
       </div>
     </main>
