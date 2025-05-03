9bf227bd290622011e18376ddaf97a3baed98800
*vivek <vrvivekofficial@gmail.com>
2025-05-03 21:24:45 +0530

    Updated hero component

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,8 +1,8 @@
-import React from "react";
+import { FaLocationArrow } from "react-icons/fa6";
+
+import MagicButton from "./MagicButton";
 import { Spotlight } from "./ui/Spotlight";
 import { TextGenerateEffect } from "./ui/TextGenerateEffect";
-import { FaLocationArrow } from "react-icons/fa6";
-import MagicButton from "./MagicButton";
 
 const Hero = () => {
   return (
@@ -36,7 +36,7 @@ const Hero = () => {
       <div className="flex justify-center relative my-20 z-10">
         <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
           <p className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
-            Dynamic Web Magic with Next.js
+            a passionate web developer
           </p>
 
           {/**
@@ -50,7 +50,7 @@ const Hero = () => {
           />
 
           <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
-            Hi! I&apos;m Vivek, a Mern stack Developer based in India.
+            Hi! I&apos;m Vivek, a MERN Stack Developer based in India.
           </p>
 
           <a href="#about">
