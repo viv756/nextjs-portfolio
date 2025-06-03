c726e601e7003ef49485aa6a30a0665ce9b9a058
*vivek <vrvivekofficial@gmail.com>
2025-06-03 23:30:14 +0530

    *

--- components/ui/GradientBg.tsx
+++ components/ui/GradientBg.tsx
@@ -40,6 +40,7 @@ export const BackgroundGradientAnimation = ({
   const [tgX, setTgX] = useState(0);
   const [tgY, setTgY] = useState(0);
   useEffect(() => {
+  if (typeof document !== "undefined") {
     document.body.style.setProperty(
       "--gradient-background-start",
       gradientBackgroundStart
@@ -56,7 +57,9 @@ export const BackgroundGradientAnimation = ({
     document.body.style.setProperty("--pointer-color", pointerColor);
     document.body.style.setProperty("--size", size);
     document.body.style.setProperty("--blending-value", blendingValue);
-  }, []);
+  }
+}, []);
+
 
   useEffect(() => {
     function move() {
