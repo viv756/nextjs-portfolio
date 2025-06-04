31bd2817d7a51b5f950b6dbc804ab94f7049a7fa
*vivek <vrvivekofficial@gmail.com>
2025-06-04 12:40:05 +0530

    *

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -1,11 +1,11 @@
-"use client"
+"use client";
 
 import { gridItems } from "@/data";
 import { BentoGrid, BentoGridItem } from "./ui/BentoGrid";
 
 const Grid = () => {
   return (
-    <section id="about" className="">
+    <section id="about">
       <BentoGrid className="w-full py-20">
         {gridItems.map((item, i) => (
           <BentoGridItem
