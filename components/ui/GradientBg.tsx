7c185fdd8729b848928acb91cd0d4f5445f5d1b7
*vivek <vrvivekofficial@gmail.com>
2025-06-04 06:11:49 +0530

    updated

--- components/ui/GradientBg.tsx
+++ components/ui/GradientBg.tsx
@@ -41,26 +41,30 @@ export const BackgroundGradientAnimation = ({
   const [tgX, setTgX] = useState(0);
   const [tgY, setTgY] = useState(0);
   useEffect(() => {
-  if (typeof document !== "undefined") {
-    document.body.style.setProperty(
-      "--gradient-background-start",
-      gradientBackgroundStart
-    );
-    document.body.style.setProperty(
-      "--gradient-background-end",
-      gradientBackgroundEnd
-    );
-    document.body.style.setProperty("--first-color", firstColor);
-    document.body.style.setProperty("--second-color", secondColor);
-    document.body.style.setProperty("--third-color", thirdColor);
-    document.body.style.setProperty("--fourth-color", fourthColor);
-    document.body.style.setProperty("--fifth-color", fifthColor);
-    document.body.style.setProperty("--pointer-color", pointerColor);
-    document.body.style.setProperty("--size", size);
-    document.body.style.setProperty("--blending-value", blendingValue);
-  }
-}, []);
-
+    if (typeof document !== "undefined") {
+      document.body.style.setProperty("--gradient-background-start", gradientBackgroundStart);
+      document.body.style.setProperty("--gradient-background-end", gradientBackgroundEnd);
+      document.body.style.setProperty("--first-color", firstColor);
+      document.body.style.setProperty("--second-color", secondColor);
+      document.body.style.setProperty("--third-color", thirdColor);
+      document.body.style.setProperty("--fourth-color", fourthColor);
+      document.body.style.setProperty("--fifth-color", fifthColor);
+      document.body.style.setProperty("--pointer-color", pointerColor);
+      document.body.style.setProperty("--size", size);
+      document.body.style.setProperty("--blending-value", blendingValue);
+    }
+  }, [
+    gradientBackgroundStart,
+    gradientBackgroundEnd,
+    firstColor,
+    secondColor,
+    thirdColor,
+    fourthColor,
+    fifthColor,
+    pointerColor,
+    size,
+    blendingValue,
+  ]);
 
   useEffect(() => {
     function move() {
@@ -69,9 +73,9 @@ export const BackgroundGradientAnimation = ({
       }
       setCurX(curX + (tgX - curX) / 20);
       setCurY(curY + (tgY - curY) / 20);
-      interactiveRef.current.style.transform = `translate(${Math.round(
-        curX
-      )}px, ${Math.round(curY)}px)`;
+      interactiveRef.current.style.transform = `translate(${Math.round(curX)}px, ${Math.round(
+        curY
+      )}px)`;
     }
 
     move();
@@ -95,16 +99,11 @@ export const BackgroundGradientAnimation = ({
       className={cn(
         "w-full h-full absolute overflow-hidden top-0 left-0 bg-[linear-gradient(40deg,var(--gradient-background-start),var(--gradient-background-end))]",
         containerClassName
-      )}
-    >
+      )}>
       <svg className="hidden">
         <defs>
           <filter id="blurMe">
-            <feGaussianBlur
-              in="SourceGraphic"
-              stdDeviation="10"
-              result="blur"
-            />
+            <feGaussianBlur in="SourceGraphic" stdDeviation="10" result="blur" />
             <feColorMatrix
               in="blur"
               mode="matrix"
@@ -120,8 +119,7 @@ export const BackgroundGradientAnimation = ({
         className={cn(
           "gradients-container h-full w-full blur-lg",
           isSafari ? "blur-2xl" : "[filter:url(#blurMe)_blur(40px)]"
-        )}
-      >
+        )}>
         <div
           className={cn(
             `absolute [background:radial-gradient(circle_at_center,_var(--first-color)_0,_var(--first-color)_50%)_no-repeat]`,
@@ -129,8 +127,7 @@ export const BackgroundGradientAnimation = ({
             `[transform-origin:center_center]`,
             `animate-first`,
             `opacity-100`
-          )}
-        ></div>
+          )}></div>
         <div
           className={cn(
             `absolute [background:radial-gradient(circle_at_center,_rgba(var(--second-color),_0.8)_0,_rgba(var(--second-color),_0)_50%)_no-repeat]`,
@@ -138,8 +135,7 @@ export const BackgroundGradientAnimation = ({
             `[transform-origin:calc(50%-400px)]`,
             `animate-second`,
             `opacity-100`
-          )}
-        ></div>
+          )}></div>
         <div
           className={cn(
             `absolute [background:radial-gradient(circle_at_center,_rgba(var(--third-color),_0.8)_0,_rgba(var(--third-color),_0)_50%)_no-repeat]`,
@@ -147,8 +143,7 @@ export const BackgroundGradientAnimation = ({
             `[transform-origin:calc(50%+400px)]`,
             `animate-third`,
             `opacity-100`
-          )}
-        ></div>
+          )}></div>
         <div
           className={cn(
             `absolute [background:radial-gradient(circle_at_center,_rgba(var(--fourth-color),_0.8)_0,_rgba(var(--fourth-color),_0)_50%)_no-repeat]`,
@@ -156,8 +151,7 @@ export const BackgroundGradientAnimation = ({
             `[transform-origin:calc(50%-200px)]`,
             `animate-fourth`,
             `opacity-70`
-          )}
-        ></div>
+          )}></div>
         <div
           className={cn(
             `absolute [background:radial-gradient(circle_at_center,_rgba(var(--fifth-color),_0.8)_0,_rgba(var(--fifth-color),_0)_50%)_no-repeat]`,
@@ -165,8 +159,7 @@ export const BackgroundGradientAnimation = ({
             `[transform-origin:calc(50%-800px)_calc(50%+800px)]`,
             `animate-fifth`,
             `opacity-100`
-          )}
-        ></div>
+          )}></div>
 
         {interactive && (
           <div
@@ -176,8 +169,7 @@ export const BackgroundGradientAnimation = ({
               `absolute [background:radial-gradient(circle_at_center,_rgba(var(--pointer-color),_0.8)_0,_rgba(var(--pointer-color),_0)_50%)_no-repeat]`,
               `[mix-blend-mode:var(--blending-value)] w-full h-full -top-1/2 -left-1/2`,
               `opacity-70`
-            )}
-          ></div>
+            )}></div>
         )}
       </div>
     </div>
