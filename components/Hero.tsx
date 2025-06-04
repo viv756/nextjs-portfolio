17ff57ec183cfb3ce5684bf9b79c5a46bbe11759
*vivek <vrvivekofficial@gmail.com>
2025-06-04 06:03:35 +0530

    changed spotlight

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -8,12 +8,12 @@ import { BackgroundLines } from "./ui/BackgroundLines";
 const Hero = () => {
   return (
     <div className="pb-20 pt-4">
-      {/* <div>
+      <div>
         <Spotlight className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen" fill="white" />
         <Spotlight className="h-[80vh] w-[50vw] top-10 left-full" fill="purple" />
         <Spotlight className="left-80 top-28 h-[80vh] w-[50vw]" fill="blue" />
-      </div> */}
-      <Spotlight/>
+      </div>
+      {/* <Spotlight/> */}
       <div
         className="h-screen w-full dark:bg-black-100 bg-white dark:bg-grid-white/[0.03] bg-grid-black-100/[0.2]
        absolute top-0 left-0 flex items-center justify-center">
