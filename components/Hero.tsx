aef01277a03e03c622cf8b98fa800c51eca93ebe
*vivek <vrvivekofficial@gmail.com>
2025-06-05 11:36:32 +0530

    fixed

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -41,7 +41,7 @@ const Hero = () => {
             <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
               Crafting User Centric Experiences.
             </p>
-            <a href="#about">
+            <a href="#projects">
               <MagicButton title="Show my work" icon={<FaLocationArrow />} position="right" />
             </a>
           </BackgroundLines>
